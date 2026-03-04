c68babbd80742e93782c4503fb655cbc787bf5cf
Nandish Rao <nandishhraoo@gmail.com>
2026-03-04 20:09:57 +0530

    feat : added react router dom
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
+import {  Route, Routes } from "react-router-dom";
 import Whiteboard from "./Components/whiteBoard";
+import LandingPage from "./Components/Landing";
+import Auth  from "./Components/Auth";
 
 function App() {
-  return <Whiteboard />;
+  return(
+    <Routes>
+       <Route path="/" element={< LandingPage/>} />
+       <Route path="/login" element={< Auth/>} />
+      <Route path="/whiteboard" element={<Whiteboard />} />
+    </Routes>
+  )
 }
 
 export default App;
