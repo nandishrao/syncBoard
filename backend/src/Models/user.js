347000365acb819805870a71f1b734298e33d434
Nandish Rao <nandishhraoo@gmail.com>
2026-03-04 19:12:16 +0530

    updated the user model
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- backend/src/Models/user.js
+++ backend/src/Models/user.js
@@ -30,7 +30,6 @@ userSchema.pre("save", async function (next) {
   this.password = await bcrypt.hash(this.password, salt);
 });
 
-// Compare password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
