2c71eb0b18ccaff8a97dee649600f3ba6b87272f
Nandish Rao <nandishhraoo@gmail.com>
2026-03-04 20:09:31 +0530

    fixed user model
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- backend/src/Models/user.js
+++ backend/src/Models/user.js
@@ -20,7 +20,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Hash password before saving
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
