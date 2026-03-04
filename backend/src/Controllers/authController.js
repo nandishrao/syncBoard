eb6bd22f077f643d5761bea27f0ec7a3b67dfcd8
Nandish Rao <nandishhraoo@gmail.com>
2026-03-04 19:12:35 +0530

    fix : updated the auth
    Signed-off-by: Nandish Rao <nandishhraoo@gmail.com>

--- backend/src/Controllers/authController.js
+++ backend/src/Controllers/authController.js
@@ -10,19 +10,15 @@ const generateToken = (id) => {
 
 export const registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-
   const userExists = await User.findOne({ email });
-
   if (userExists) {
     return res.status(400).json({ message: "User already exists" });
   }
-
   const user = await User.create({
     name,
     email,
     password,
   });
-
   if (user) {
     res.status(201).json({
       _id: user._id,
@@ -34,9 +30,7 @@ export const registerUser = async (req, res) => {
 };
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email });
-
   if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
